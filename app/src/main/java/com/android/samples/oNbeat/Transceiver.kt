b7192359f19b1fcc9a7dad6a979d6f7a1da85318
Lukas Nennstiel <lukas.nennstiel@mercedes-benz.com>
2023-05-03 15:56:53 +0200

    Change client to socket

--- app/src/main/java/com/android/samples/oNbeat/Transceiver.kt
+++ app/src/main/java/com/android/samples/oNbeat/Transceiver.kt
@@ -22,33 +22,38 @@ fun main(args: Array<String>) {
 
 }
 
-class ClientHandler(client: Socket) {
-    private val client: Socket = client
-    private val reader: Scanner = Scanner(client.getInputStream())
-    private val writer: OutputStream = client.getOutputStream()
-    private val calculator: Calculator = Calculator()
+class ClientHandler(socket: Socket) {
+    // rename client -> socket
+    private val socket: Socket = socket
+    private val reader: Scanner = Scanner(socket.getInputStream())
+    private val writer: OutputStream = socket.getOutputStream()
     private var running: Boolean = false
 
     fun run() {
         running = true
         // Welcome message
-        write("Welcome to the server!\n" +
-                "To Exit, write: 'EXIT'.\n" +
-                "To use the calculator, input two numbers separated with a space and an operation in the ending\n" +
-                "Example: 5 33 multi\n" +
-                "Available operations: 'add', 'sub', 'div', 'multi'")
+        write("Welcome to the server!")
+
+        // Alternativer Ansatz
+        dis = DataInputStream(socket.getInputStream())
 
         while (running) {
             try {
+                // Alternativer Ansatz
+                // -----------------------------------------------------------------------------------------------
+                if (dis.available() > 0) {
+                    // ToDo "Bufferedreader?"
+                }
+
+
+
+                // -----------------------------------------------------------------------------------------------
                 val text = reader.nextLine()
                 if (text == "EXIT"){
                     shutdown()
                     continue
                 }
-
-                val values = text.split(' ')
-                val result = calculator.calculate(values[0].toInt(), values[1].toInt(), values[2])
-                write(result)
+                write("acknowledged")
             } catch (ex: Exception) {
                 // TODO: Implement exception handling
                 shutdown()
@@ -65,35 +70,8 @@ class ClientHandler(client: Socket) {
 
     private fun shutdown() {
         running = false
-        client.close()
+        socket.close()
         println("${client.inetAddress.hostAddress} closed the connection")
     }
-
-}
-
-class Calculator {
-
-    fun calculate(a: Int, b: Int, operation: String): String {
-        when (operation) {
-            "add" -> return calc(a, b, ::add).toString()
-            "sub" -> return calc(a, b, ::sub).toString()
-            "div" -> return calc(a.toDouble(), b.toDouble(), ::div).toString()
-            "multi" -> return calc(a, b, ::multi).toString()
-            else -> {
-                return "Something whent wrong"
-            }
-        }
-    }
-
-    // A Calculator (functional programming)
-    private fun <T> calc(a: T, b: T, operation: (T, T) -> T): T {
-        return operation(a, b)
-    }
-
-    private fun add(a: Int, b: Int): Int = a + b
-    private fun sub(a: Int, b: Int): Int = a - b
-    private fun div(a: Double, b: Double): Double = a / b
-    private fun multi(a: Int, b: Int): Int = a * b
-
 
 }
